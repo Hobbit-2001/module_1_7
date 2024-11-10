3a704b438d0e9ea36cc8ada7a3b6458b18b3b017
Alexandr <70943203+Hobbit-2001@users.noreply.github.com>
2024-11-10 14:38:51 +0300

    Initial commit

---
+++ module_1_7.py
@@ -0,0 +1,7 @@
+immutable_var = 1, 2, True, 'Hello'
+#immutable_var[0] = 'Hello" Неизменяемый тип данных!!!!!!!!!!!!!!!!!!!!!
+print(immutable_var)
+
+mutable_list = [1, 2, 'Name', False]
+mutable_list[0] = 'Hello'
+print(mutable_list)
